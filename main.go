11c641851fac8410aab83c0b98596361875d1566
kopher <kopher1601@gmail.com>
2025-06-15 15:12:49 +0900

    Add errgroup for concurrent HTTP server handling, implement run function, and add tests for server response

--- main.go
+++ main.go
@@ -1,18 +1,47 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
-func main() {
-	err := http.ListenAndServe(":18080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
-	}),
-	)
-	if err != nil {
-		fmt.Printf("failed to terminate server: %v", err)
-		os.Exit(1)
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
+		}),
 	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+	// 다른 고루틴에서 HTTP 서버를 실행한다.
+	eg.Go(func() error {
+		// http.ErrServerClosed 는
+		// http.Server.Shutdown() 가 정상 종료된 것을 나타내므로 이상 처리가 아니다.
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close: %v", err)
+			return err
+		}
+		return nil
+	})
+
+	// 채널로부터의 알림(종료 알림)을 기다린다.
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	// Go 메서드로 실행한 다른 고루틴의 종료를 기다린다.
+	return eg.Wait()
+}
+
+func main() {
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
+	}
+
 }
