987e02b1ff9ad2c41161bfa02cc77c2ad14fe69b
kopher <kopher1601@gmail.com>
2025-06-15 16:42:18 +0900

    Refactor application to use configuration package for environment variables, update Docker settings for port mapping, and add Makefile for build and deployment tasks. Introduce tests for configuration loading.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go_todo_app
 
 go 1.23.0
 
-require golang.org/x/sync v0.15.0
+require (
+	github.com/caarlos0/env/v10 v10.0.0
+	golang.org/x/sync v0.15.0
+)
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"go_todo_app/config"
 	"log"
 	"net"
 	"net/http"
@@ -11,7 +12,19 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-func run(ctx context.Context, l net.Listener) error {
+func run(ctx context.Context) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	}
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with %s", url)
+
 	s := &http.Server{
 		// 인수로 받은 net.Listener 를 이용하므로 Addr 필드는 지정하지 않는다.
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -42,17 +55,7 @@ func run(ctx context.Context, l net.Listener) error {
 }
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Println("os.Args>>>", os.Args)
-		log.Printf("need port number\n")
-		os.Exit(1)
-	}
-	p := os.Args[1]
-	l, err := net.Listen("tcp", ":"+p)
-	if err != nil {
-		log.Fatalf("failed to listen port %s: %v", p, err)
-	}
-	if err := run(context.Background(), l); err != nil {
+	if err := run(context.Background()); err != nil {
 		log.Printf("failed to terminate server: %v", err)
 		os.Exit(1)
 	}
