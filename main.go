65d4f73ff8982835c6313506f9b7aeb0509538df
kopher <kopher1601@gmail.com>
2025-06-15 15:30:02 +0900

    Refactor run function to accept net.Listener for dynamic port handling and update tests to reflect changes in server initialization.

--- main.go
+++ main.go
@@ -4,14 +4,16 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 
 	"golang.org/x/sync/errgroup"
 )
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: ":18080",
+		// 인수로 받은 net.Listener 를 이용하므로 Addr 필드는 지정하지 않는다.
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
 		}),
@@ -22,7 +24,7 @@ func run(ctx context.Context) error {
 	eg.Go(func() error {
 		// http.ErrServerClosed 는
 		// http.Server.Shutdown() 가 정상 종료된 것을 나타내므로 이상 처리가 아니다.
-		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
 			log.Printf("failed to close: %v", err)
 			return err
 		}
@@ -40,8 +42,17 @@ func run(ctx context.Context) error {
 }
 
 func main() {
-	if err := run(context.Background()); err != nil {
-		log.Printf("failed to terminate server: %v", err)
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
 	}
-
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+	if err := run(context.Background(), l); err != nil {
+		log.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
+	}
 }
